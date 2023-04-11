9abec45a72e08eecc870d3d7210a6d8fbb2cf224
Peter Orchard <porchard@umich.edu>
2023-04-11 12:26:31 -0400

    Get gene bodies or TSS from GTF file

--- plutils/gtf.py
+++ plutils/gtf.py
@@ -2,6 +2,7 @@
 
 import re
 import pandas as pd
+import numpy as np
 
 
 def gtf_to_df(gtf):
@@ -19,3 +20,46 @@ def parse_attributes(a, regex='[; ]*(.*?) "(.*?)"'):
     """
     x = [m.groups() for m in re.finditer(regex, a)]
     return {key: val for key, val in x}
+
+
+def gtf_to_tss(gtf, feature_id='gene_id'):
+    """
+    Given a GTF file, create a BED6-style DataFrame of TSS.
+
+    Input:
+    gtf: Path to GTF file
+    feature_id: Attribute to use for labeling TSS (usually e.g. gene_id, gene_name, or transcript_id)
+
+    Output:
+    pandas DataFrame of TSS (chrom, start, end, feature_id, ., strand)
+    """
+
+
+    df = gtf_to_df(gtf)
+    df = df[df.feature=='transcript']
+    df['tss'] = np.where(df.strand == '+', df.start, df.end)
+    df['tss_start'] = df.tss - 1  # BED indexes from 0
+    df['tss_end'] = df.tss
+    df['id'] = df.attributes.map(lambda x: parse_attributes(x)[feature_id])
+    df['score'] = '.'
+    return df[['chrom', 'tss_start', 'tss_end', 'id', 'score', 'strand']].rename(columns=lambda x: x.replace('tss_', ''))
+
+
+def gtf_to_gene_bodies(gtf, id='gene_id'):
+    """
+    Given a GTF file, create a BED6-style DataFrame of gene bodies.
+
+    Input:
+    gtf: Path to GTF file
+    feature_id: Attribute to use for labeling TSS (usually e.g. gene_id or gene_name)
+
+    Output:
+    pandas DataFrame of gene bodies (chrom, start, end, feature_id, ., strand)
+    """
+
+    df = gtf_to_df(gtf)
+    df = df[df.feature=='gene']
+    df['id'] = df.attributes.map(lambda x: parse_attributes(x)[id])
+    df['start'] = df.start - 1 # BED indexes from 0
+    df['score'] = '.'
+    return df[['chrom', 'start', 'end', 'id', 'score', 'strand']]
